4a88e596bb2d00b0c9a21cb7ed83ab142fcbe794
grisoni77 <cristiano.cucco@gmail.com>
2012-07-30 18:16:35 +0200

    changes to admin

--- src/Fc/AdminBundle/Controller/ChampionshipAdminController.php
+++ src/Fc/AdminBundle/Controller/ChampionshipAdminController.php
@@ -4,6 +4,8 @@ namespace Fc\AdminBundle\Controller;
 
 //use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Description of SeasonAdminController
@@ -12,8 +14,13 @@ use Sonata\AdminBundle\Controller\CRUDController as Controller;
  */
 class ChampionshipAdminController extends Controller {
 
+    /**
+     * @Template()
+     * @return \Symfony\Component\HttpFoundation\Response 
+     */
     public function importAction()
     {
-        
+        //return new Response("<p>test import</p>");
+        return array();
     }
 }
