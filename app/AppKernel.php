063bca8f4ca0e9a05da4b4e4eddf9ebc0deee74f
Cristiano Cucco <cristiano.cucco@gmail.com>
2012-09-08 01:47:08 +0200

    testing botstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -33,6 +33,7 @@ class AppKernel extends Kernel
             //new Symfony\Bundle\DoctrineFixturesBundle\DoctrineFixturesBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Fc\SiteBundle\FcSiteBundle(),
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
